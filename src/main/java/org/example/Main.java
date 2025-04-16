e3cf09910ffbdaf4c1b4cf94729d49e1d8639e8c
Дмитрий <iii.dimon@yandex.ru>
2025-04-16 09:43:53 +0400

    if заменен на switch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,52 @@
+package org.example;
+
+import java.lang.reflect.Array;
 import java.util.Scanner;
+import static java.lang.System.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public static class Main {
-    public static String_calc(String input) {
+public class Main {
 
-        String[] arr = str.split(" ");
+    public static void main(String[] args) throws Exception {
 
-        /*public int add ( int a, int b) {
-            return a + b;
-        }
+        out.println("Добро пожаловать в консольное приложение “Калькулятор”!");
+        out.println("Введите строку с арифметическим выражением операций (+, -, * или /) между двумя целыми числами от 1 до 10 через пробелы:");
 
-        public int minus ( int a, int b) {
-            return a - b;
-        }
-
-        public int multi ( int a, int b) {
-            return a * b;
-        }
-
-        public int div ( int a, int b) {
-            return a / b;
-        }*/
-
-        System.out.println(arr);
-    }
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(in);
 
         String str = sc.nextLine();
 
-        String_calc(str);
+        out.println(calc(str));
+
+    }
+
+    public static String calc(String input) throws Exception {
+
+        String[] arr = input.split(" ");
+        int length = Array.getLength(arr);
+
+        if (length != 3) {
+            if (length < 3) {
+                throw new Exception("т.к. строка не является математической операцией");
+            } else {
+                throw new Exception("т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
+            }
+        }
+
+        byte a = Byte.parseByte(arr[0]);
+        byte b = Byte.parseByte(arr[2]);
+        String operation = String.valueOf(arr[1]);
+        byte result;
+
+        if (a < 1 || a > 10 || b < 1 || b > 10) throw new Exception("введите число от 1 до 10");
+
+        result = switch (operation) {
+            case "+" -> (byte) (a + b);
+            case "-" -> (byte) (a - b);
+            case "*" -> (byte) (a * b);
+            case "/" -> (byte) (a / b);
+            default -> throw new Exception();
+        };
+
+        return String.valueOf(result);
     }
 }
