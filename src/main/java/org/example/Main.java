49eb3958fff722448d418694d0c5fc59cf31f187
Дмитрий <iii.dimon@yandex.ru>
2025-04-16 08:40:53 +0400

    первый

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public static class Main {
+    public static String_calc(String input) {
+
+        String[] arr = str.split(" ");
+
+        /*public int add ( int a, int b) {
+            return a + b;
+        }
+
+        public int minus ( int a, int b) {
+            return a - b;
+        }
+
+        public int multi ( int a, int b) {
+            return a * b;
+        }
+
+        public int div ( int a, int b) {
+            return a / b;
+        }*/
+
+        System.out.println(arr);
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        String_calc(str);
+    }
+}
